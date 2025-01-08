338952322eb403701464d5d55f8b18558295fb70
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-01-08 09:26:07 +0100

    Übung 3

--- src/main/java/de/lubowiecki/tag4/FahrzeugTest.java
+++ src/main/java/de/lubowiecki/tag4/FahrzeugTest.java
@@ -1,6 +1,6 @@
 package de.lubowiecki.tag4;
 
-import de.lubowiecki.mylib.Fahrzeug;
+//import de.lubowiecki.mylib.Fahrzeug;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,6 +10,7 @@ import java.util.List;
 public class FahrzeugTest {
 
     public static void main(String[] args) {
+        /*
         Fahrzeug f1 = new Fahrzeug("HH:AB123", "VW", "Polo", 2020);
         Fahrzeug f2 = new Fahrzeug("HH:XY223", "Ford", "Capri", 2024);
         Fahrzeug f3 = new Fahrzeug("A:AB229", "Renault", "Clio", 2022);
@@ -26,6 +27,7 @@ public class FahrzeugTest {
         Collections.sort(list, Comparator.comparing(Fahrzeug::getMarke)); // Methoden-Referenz
 
         System.out.println(list);
+        */
 
     }
 }
